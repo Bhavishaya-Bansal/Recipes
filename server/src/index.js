1284e2c58ad683eda84978043080d524dcf51fbd
bansalbhavishaya <bhavishayabansal31@gmail.com>
2024-11-24 13:53:27 +0530

    updated cors link

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // app.use(cors());
 app.use((req, res, next) => {
 
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://recipes-570a.onrender.com/auth/login', 'https://recipes-570a.onrender.com/auth/register', 'https://recipes-570a.onrender.com/recipes', 'https://recipes-570a.onrender.com/recipes', 'https://recipes-570a.onrender.com/recipes/savedRecipes/ids/', 'https://recipes-570a.onrender.com/recipes', 'https://recipes-570a.onrender.com/recipes/savedRecipes/', 'https://recipes-frontend-7yts.onrender.com/', 'https://recipes-570a.onrender.com');
 
   next();
 
