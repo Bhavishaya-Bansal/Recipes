0c0f72330af09b35619e81df6fe155c46c4c993f
bansalbhavishaya <bhavishayabansal31@gmail.com>
2024-11-24 12:54:49 +0530

    Completed commit

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,25 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+// import { recipesRouter } from "./routes/recipes.js";
+// import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
+import { userRouter } from "./routes/users.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://bhavishaya:Bhavishaya031@recipes.yss3k.mongodb.net/recipes?retryWrites=true&w=majority&appName=recipes")
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
-app.listen(3001, ()=> console.log("Server is up and running"));
+mongoose.connect(
+  "mongodb+srv://bhavishaya:Bhavishaya031@recipes.yss3k.mongodb.net/recipes?retryWrites=true&w=majority&appName=recipes",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
+app.listen(3001, () => console.log("Server started"));
